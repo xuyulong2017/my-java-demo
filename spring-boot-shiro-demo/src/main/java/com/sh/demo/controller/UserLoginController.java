040a8232c2b4819662ebc578820d4c53b21bae51
Sans <761502235@qq.com>
2020-01-06 21:38:03 +0800

    增加ShiroDemo用户加密演示

--- spring-boot-shiro-demo/src/main/java/com/sh/demo/controller/UserLoginController.java
+++ spring-boot-shiro-demo/src/main/java/com/sh/demo/controller/UserLoginController.java
@@ -1,18 +1,23 @@
 package com.sh.demo.controller;
 
+import com.sh.demo.common.util.SHA256Util;
 import com.sh.demo.common.util.ShiroUtils;
 import com.sh.demo.core.entity.SysUserEntity;
+import com.sh.demo.core.entity.SysUserRoleEntity;
+import com.sh.demo.core.service.SysUserRoleService;
+import com.sh.demo.core.service.SysUserService;
+import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.IncorrectCredentialsException;
 import org.apache.shiro.authc.LockedAccountException;
 import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * @Description 用户登录
@@ -22,6 +27,11 @@ import java.util.Map;
 @RestController
 @RequestMapping("/userLogin")
 public class UserLoginController {
+
+    @Autowired
+    private SysUserService sysUserService;
+    @Autowired
+    private SysUserRoleService sysUserRoleService;
 
     /**
      * 登录
@@ -36,7 +46,7 @@ public class UserLoginController {
             //验证身份和登陆
             Subject subject = SecurityUtils.getSubject();
             UsernamePasswordToken token = new UsernamePasswordToken(sysUserEntity.getUsername(), sysUserEntity.getPassword());
-            //验证成功进行登录操作
+            //进行登录操作
             subject.login(token);
         }catch (IncorrectCredentialsException e) {
             map.put("code",500);
@@ -70,6 +80,38 @@ public class UserLoginController {
         Map<String,Object> map = new HashMap<>();
         map.put("code",500);
         map.put("msg","未登录");
+        return map;
+    }
+
+
+    /**
+     * 添加一个用户演示接口
+     * 这里仅作为演示不加任何权限和重复查询校验
+     * @Author Sans
+     * @CreateTime 2020/1/6 9:22
+     */
+    @RequestMapping("/testAddUser")
+    public Map<String,Object> testAddUser(){
+        // 设置基础参数
+        SysUserEntity sysUser = new SysUserEntity();
+        sysUser.setUsername("user1");
+        sysUser.setState("NORMAL");
+        // 随机生成盐值
+        String salt = RandomStringUtils.randomAlphanumeric(20);
+        sysUser.setSalt(salt);
+        // 进行加密
+        String password ="123456";
+        sysUser.setPassword(SHA256Util.sha256(password, sysUser.getSalt()));
+        // 保存用户
+        sysUserService.save(sysUser);
+        // 保存角色
+        SysUserRoleEntity sysUserRoleEntity = new SysUserRoleEntity();
+        sysUserRoleEntity.setUserId(sysUser.getUserId()); // 保存用户完之后会把ID返回给用户实体
+        sysUserRoleService.save(sysUserRoleEntity);
+        // 返回结果
+        Map<String,Object> map = new HashMap<>();
+        map.put("code",0);
+        map.put("msg","添加成功");
         return map;
     }
 }
